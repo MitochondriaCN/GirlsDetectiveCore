f0971e5954e6b8d554130d70ba8d5d0ebc2fcf0f
线粒体XianlitiCN <mitochondriacn@gmail.com>
2022-02-14 18:46:25 +0800

    基础

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using Telegram.Bot;
+using System.IO;
+using GirlsDetectiveCore.Tools;
+using rdr = GirlsDetectiveCore.Tools.ConsoleRenderer;
 
-var botClient = new TelegramBotClient("5204393448:AAE8JWbcQaRVIcjKqFC7BwkDkcEtW8daTKA");
-var me = await botClient.GetMeAsync();
+try
+{
+    var botClient = new TelegramBotClient(File.ReadAllText("token.gdt"));
+}
+catch (FileNotFoundException e)
+{
+    rdr.TitlePara("启动错误");
+    rdr.Para("未找到 token.gdt 文件，请在软件根目录创建它，并在其中写入机器人 token 。");
+    return;
+}
---
+++ Tools/ConsoleRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirlsDetectiveCore.Tools
+{
+    internal static class ConsoleRenderer
+    {
+        internal static void TitlePara(string title)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("            " + title + "            ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        internal static void Para(string text)
+        {
+            Console.ResetColor();
+            Console.WriteLine(text);
+        }
+    }
+}
